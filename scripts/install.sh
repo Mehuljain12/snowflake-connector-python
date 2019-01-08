fd6692fdac0c804e3cd3b4fe9162aabb3ce461fd
Shige Takeda <smtakeda@gmail.com>
2019-01-08 17:11:07 +0000

    added appveyor

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,6 +3,28 @@
 # Install Snowflake Python Connector
 #
 set -o pipefail
+
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+    brew update
+    brew install openssl readline sqlite3 xz zlib
+    brew outdated pyenv || brew upgrade pyenv
+    brew install pyenv-virtualenv
+    pyenv install ${PYTHON_VERSION}
+    export PYENV_VERSION=$PYTHON
+    export PATH="${HOME}/.pyenv/shims:${PATH}"
+    if [[ $PYTHON_VERSION == "2.7"* ]]; then
+        pip install -U virtualenv
+        python -m virtualenv venv
+    else
+        python3 -m venv venv
+    fi
+else
+    sudo apt-get update
+    pip install -U virtualenv
+    python -m virtualenv venv
+fi
+openssl aes-256-cbc -k "$super_secret_password" -in parameters.py.enc -out test/parameters.py -d
+
 source ./venv/bin/activate
 pip install numpy
 pip install pytest pytest-cov pytest-rerunfailures
